951c0c47932b06925ec0e1648f18e73c2cde6cda
Matija Srnec <srnec.matija@gmail.com>
2020-11-21 17:09:41 +0100

    modified app component

--- my-first-angular-project/src/app/app.component.ts
+++ my-first-angular-project/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-angular-project';
+  title = 'This is my first App :)';
+  firstName = "Joe";
+  lastName = "Doe";
 }
